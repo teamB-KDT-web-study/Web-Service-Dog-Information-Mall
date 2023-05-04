3d0835a770c5254bcf990ed6b17e514e5b0b68ec
seheeeJeong <usung7888@naver.com>
2023-05-04 18:05:31 +0900

    add: slick

---
+++ client/src/components/SlickPrevArrow.js
@@ -0,0 +1,12 @@
+function SlickPrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default SlickPrevArrow;
